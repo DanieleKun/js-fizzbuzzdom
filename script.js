94850037c70063876c026416a4db7ec7c488be23
Daniele <pucciodaniele95@gmail.com>
2022-05-05 16:42:18 +0200

    add css and finish

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100.
 // Per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
+
+const myList = document.querySelector(".list")
+
+
 // Stampa i numeri da 1 a 100
- for (let i = 1; i <= 100; i++) {
-     // console.log(i);
+for (let i = 1; i <= 100; i++) {
     let myCard;
+    //  Modifica i multipli di 3 e 5
+    if (i % 3 === 0 && i % 5 !== 0) {
+        myCard = `<li class="card bg_fizz">fizz</li>`; 
+    } else if (i % 5 === 0 && i % 3 !== 0) {
+        myCard = `<li class="card bg_buzz">buzz</li>`;
+    } else if (i % 3 === 0 && i % 5 === 0) {
+        myCard = `<li class="card bg_fizzbuzz">fizzbuzz</li>`;
+    } else {
+        myCard = `<li class="card bg_number">${i}</li>`;
+    }
+    // console.log(myCard);
 
-     if (i % 3 === 0 && i % 5 !== 0) {
-         myCard = 'fizz';
-     } else if (i % 5 === 0 && i % 3 !== 0) {
-         myCard = 'buzz';
-     } else if (i % 3 === 0 && i % 5 === 0) {
-         myCard = 'fizzbuzz';
-     } else {
-         myCard = i
-     }
+    
+    myList.innerHTML += myCard;
+    
+}
 
-     console.log(myCard);
- }
 
